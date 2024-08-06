f2bd00fc73b282f8e2fcc89eb163228ec28373b7
André <48385244+estavadormir@users.noreply.github.com>
2024-08-06 23:30:07 +0100

    (chores): removed useless test.

--- src/utils/crawlToPdf.test.ts
+++ src/utils/crawlToPdf.test.ts
@@ -1,11 +1,6 @@
 import { test, expect } from "bun:test";
 import { crawlToPdf } from "./crawlToPdf";
 
-test("crawlToPdf should return a pdf file", async () => {
-  const pdf = await crawlToPdf("https://www.google.com/", "test.pdf");
-  expect(pdf).toBeDefined();
-});
-
 test("crawlToPdf should throw an error if the url is invalid", async () => {
   await expect(crawlToPdf("invalid url", "test.pdf")).rejects.toThrow(
     "Invalid URL"
